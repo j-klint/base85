58165d5d1a4d880ad95b363ab40de5cee6fe34e6
J. Klint <jaakko.klint@edunord.se>
2023-10-11 16:50:19 +0200

    Alkeellinen command line -parsiminen

--- main.cpp
+++ main.cpp
@@ -1,28 +1,97 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main(int argc, char** argv)
+struct Parameters
+{
+	char* inputFile{ nullptr };
+	int wrap{ 75 };
+	bool decode{ false };
+};
+
+static constexpr uint32_t alphabetSize{ 85 };
+int Encode(std::istream& instream, size_t wrap);
+int Decode(std::istream& instream);
+Parameters ParseArgs(int argc, char** argv);
+
+int main(int argc, char** argv) try
 {
 	std::ios::sync_with_stdio(false); // toivotaan, että tämä nopeuttaa aiheuttamatta haittaa
-	constexpr uint32_t alphabetSize{85};
 	std::ifstream infile;
 
-	if ( argc == 2 )
+	Parameters args = ParseArgs(argc, argv);
+
+	if ( args.inputFile )
 	{
-		infile.open(argv[1], std::ios::binary);
+		infile.open(args.inputFile, std::ios::binary);
 		if ( !infile.good() )
 		{
 			std::cerr << "Unable to open input file.\n";
 			return 1;
 		}
 	}
-	// else
-	// {
-	// 	std::cerr << "No input file specified. Reading from std::cin.\n";
-	// }
 
-	std::istream& instream{ argc == 2 ? infile : std::cin };
-	size_t wrap { 75 }, charsOnThisLine{ 0 };
+	std::istream& instream{ args.inputFile ? infile : std::cin };
+
+	if ( args.decode )
+		return Decode(instream);
+	else
+		return Encode(instream, args.wrap);
+}
+catch ( std::string& v )
+{
+	std::cerr << "Error: " << v;
+	return 1;
+}
+catch(const std::exception& e)
+{
+	std::cerr << "Exception: " << e.what();
+	return 1;
+}
+
+Parameters ParseArgs(int argc, char** argv)
+{
+	Parameters retval;
+	std::string input;
+
+	for ( int i = 1; i < argc; ++i )
+	{
+		input = argv[i];
+
+		if ( input == "-d" )
+		{
+			retval.decode = true;
+		}
+		else if ( input == "-w" )
+		{
+			bool error{ argc <= i + 1 };
+			if ( !error )
+			{
+				try
+				{
+					retval.wrap = std::max(0, std::stoi(argv[++i]));
+				}
+				catch(const std::exception&)
+				{
+					error = true;
+				}
+			}
+			
+			if ( error )
+				throw std::string{ "Couldn't parse -w parameter." };
+		}
+		else
+		{
+			retval.inputFile = argv[i];
+		}
+	}
+
+	return retval;
+}
+
+int Encode(std::istream& instream, size_t wrap)
+{
+	size_t charsOnThisLine{ 0 };
 
 	auto OutputCharacter = [wrap, &charsOnThisLine](char c)
 	{
@@ -71,5 +140,11 @@ int main(int argc, char** argv)
 		}
 	}
 
+	return 0;
+}
+
+int Decode(std::istream& )//instream)
+{
+	std::cerr << "Decode function not done yet.\n";
 	return 0;
 }
